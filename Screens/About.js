9894f47c9ea33e5729407c321a169341f89a1927
mikeysweeney <78807853+mikeysweeney@users.noreply.github.com>
2021-04-01 11:41:06 +0100

    Added an about page and a settings page

--- Screens/About.js
+++ Screens/About.js
@@ -3,13 +3,6 @@ import React from 'react';
 import { Button, StyleSheet, Text, View, BackHandler, Pressable } from 'react-native';
 
 export default class AboutScreen extends React.Component{
-    componentDidMount()//Prevents the user from going back to intro/loading page
-    {
-    BackHandler.addEventListener('hardwareBackPress', () => true); 
-    }
-    buttonPress = () => {
-        this.props.navigation.navigate('Intro');
-    }
     render(){
         return(
             <View style={styles.container}>
@@ -18,7 +11,17 @@ export default class AboutScreen extends React.Component{
                 <Text style={styles.text}>
                     Hi and welcome to LanguagePal
                     {"\n\n"}
-                    These are the settings
+                    LanguagePal is a language tool to assist people
+                    with learning new languages and finding new ways to
+                    easily learn similar words between languages. 
+                    {"\n\n"}
+                    In comparative linguistics, lexical relationship or genealogical proximity is the connection between languages, either the members of the same language family or the tradeoff between the languages due to cultural interactions. 
+                    A language similarity would highlight the overlap between vocabularies in lexical, phonetical, and semantic aspects. Furthermore, it would indicate mutual intelligibility of the two languages up to an extent.
+                    Such an analysis can reveal critical information about how the words evolve as they transition between languages. Moreover, the speakers of a given language may find it easier to learn similar strongly correlated languages. 
+                    Such information can be used to emphasize the similarities between the languages and make recommendations to users who are willing to learn new languages. 
+                    {"\n\n"}
+                    Learn thousands of common words between two languages 
+                    of your choice, helping you learn a new language quicker.
                 </Text>
                 </View>
             </View>
