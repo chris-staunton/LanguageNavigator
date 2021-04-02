032b6f93e7a7db5cd9ee78ff01dd621f179d1dcb
Daniel Whelan <dpw0990@gmail.com>
2021-04-02 16:23:55 +0100

    Added functionality for Min PQ of 200 words

--- backend/LanguageNavigator/TranslationAlgo.py
+++ backend/LanguageNavigator/TranslationAlgo.py
@@ -1,12 +1,66 @@
 from google_trans_new import google_translator
 from english_words import english_words_lower_alpha_set
+from pip._vendor.msgpack.fallback import xrange
+import heapq
 
 translator = google_translator()
 wordDict = {}
-for i in english_words_lower_alpha_set:
-    trans = translator.translate(i, lang_tgt="fr")
-    if isinstance(trans, list):
-        wordDict[i] = trans[0]
-    else:
-        wordDict[i] = trans
-    print(i + " --> " + wordDict[i])
+
+def get_key(dictionary, n=0):
+    if n < 0:
+        n = 0
+    for i, key in enumerate(dictionary.keys()):
+        if i == n:
+            return key
+
+#@author: Conall
+def levenshteinDistance(seq1, seq2):
+    size_x = len(seq1) + 1
+    size_y = len(seq2) + 1
+    matrix = [(0*size_y) for i in range(size_x)]
+    #Set X's and Y's to 0
+    for x in xrange(size_x):
+        matrix [x, 0] = x
+    for y in xrange(size_y):
+        matrix [0, y] = y
+
+    #Go through each letter of each word and determine a number
+    for x in xrange(1, size_x):
+        for y in xrange(1, size_y):
+
+            if seq1[x-1] == seq2[y-1]:
+                matrix [x,y] = min(matrix[x-1, y] + 1, matrix[x-1, y-1], matrix[x, y-1] + 1)
+            else:
+                matrix [x,y] = min(matrix[x-1,y] + 1, matrix[x-1,y-1] + 1, matrix[x,y-1] + 1)
+    print (matrix)
+    print("The distance is: ", matrix[size_x - 1, size_y - 1])
+    return (matrix[size_x - 1, size_y - 1])
+
+
+if __name__ == '__main__':
+    for i in english_words_lower_alpha_set:
+        trans = translator.translate(i, lang_tgt="fr")
+        if isinstance(trans, list):
+            wordDict[i] = trans[0]
+        else:
+            wordDict[i] = trans
+        print(i + " --> " + wordDict[i])
+        if len(wordDict) >= 950:
+            break
+    heap = []
+    for i in range(len(wordDict)):
+        key = get_key(wordDict, i)
+        trans = wordDict[key]
+        x = levenshteinDistance(key, trans)
+        if len(heap) <= 200:
+            heapq.heappush(heap, (key, trans))
+        else:
+            del heap[len(heap)-1]
+            heapq.heappush(key,trans)
+
+
+
+
+
+
+
