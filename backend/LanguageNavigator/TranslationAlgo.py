d702b25e9dad8cf5b458b054c6b3149b4481d371
Daniel Whelan <dpw0990@gmail.com>
2021-04-05 17:05:53 +0100

    Fixed bugs with Min PQ and levenshtein distance algorithm, now produces 200 similar words, filer still needs work

--- backend/LanguageNavigator/TranslationAlgo.py
+++ backend/LanguageNavigator/TranslationAlgo.py
@@ -17,29 +17,28 @@ def get_key(dictionary, n=0):
 def levenshteinDistance(seq1, seq2):
     size_x = len(seq1) + 1
     size_y = len(seq2) + 1
-    matrix = [(0*size_y) for i in range(size_x)]
+    matrix = [[0 for i in range(size_y)] for i in range(size_x)]
     #Set X's and Y's to 0
-    for x in xrange(size_x):
-        matrix [x, 0] = x
-    for y in xrange(size_y):
-        matrix [0, y] = y
+    for x in range(size_x):
+        matrix[x][0] = x
+    for y in range(size_y):
+        matrix[0][y] = y
 
     #Go through each letter of each word and determine a number
-    for x in xrange(1, size_x):
-        for y in xrange(1, size_y):
-
+    for x in range(1, size_x):
+        for y in range(1, size_y):
             if seq1[x-1] == seq2[y-1]:
-                matrix [x,y] = min(matrix[x-1, y] + 1, matrix[x-1, y-1], matrix[x, y-1] + 1)
+                matrix [x][y] = min(matrix[x-1][y] + 1, matrix[x-1][y-1], matrix[x][y-1] + 1)
             else:
-                matrix [x,y] = min(matrix[x-1,y] + 1, matrix[x-1,y-1] + 1, matrix[x,y-1] + 1)
+                matrix [x][y] = min(matrix[x-1][y] + 1, matrix[x-1][y-1] + 1, matrix[x][y-1] + 1)
     print (matrix)
-    print("The distance is: ", matrix[size_x - 1, size_y - 1])
-    return (matrix[size_x - 1, size_y - 1])
+    print("The distance is: ", matrix[size_x - 1][size_y - 1])
+    return (matrix[size_x - 1][size_y - 1])
 
 
 if __name__ == '__main__':
     for i in english_words_lower_alpha_set:
-        trans = translator.translate(i, lang_tgt="fr")
+        trans = translator.translate(i, lang_tgt="es")
         if isinstance(trans, list):
             wordDict[i] = trans[0]
         else:
@@ -50,13 +49,16 @@ if __name__ == '__main__':
     heap = []
     for i in range(len(wordDict)):
         key = get_key(wordDict, i)
-        trans = wordDict[key]
+        trans = wordDict[key].lower()
         x = levenshteinDistance(key, trans)
-        if len(heap) <= 200:
+        if x == 0:
+            continue
+        elif len(heap) <= 200:
             heapq.heappush(heap, (key, trans))
         else:
             del heap[len(heap)-1]
-            heapq.heappush(key,trans)
+            heapq.heappush(heap, (key,trans))
+    print(heap)
 
 
 
